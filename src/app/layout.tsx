b6de4f2f22478a72aa99e5365e794b4d99f062be
fady emad <fadyemad933@gmail.com>
2025-11-27 01:27:16 +0200

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,95 @@
 import { RootProvider } from 'fumadocs-ui/provider/next';
-import './global.css';
-import { Inter } from 'next/font/google';
+import '../styles/global.css';
+import { Inter, Syne } from 'next/font/google';
+import { Metadata } from 'next';
 
 const inter = Inter({
   subsets: ['latin'],
 });
+const syne = Syne({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Fixel UI',
+    default: 'Fixel UI - Animated React Components',
+  },
+  description:
+    'Fully animated, open-source component distribution built with React, TypeScript, Tailwind CSS, Motion and Shadcn CLI. Browse a list of components you can install, modify, and use in your projects.',
+  keywords: [
+    'Fixel UI',
+    'React',
+    'TypeScript',
+    'Tailwind CSS',
+    'Framer Motion',
+    'Open-source components',
+    'Animated UI components',
+    'UI library',
+  ],
+  icons: [
+    {
+      rel: 'icon',
+      type: 'image/svg',
+      sizes: '32x32',
+      url: '/favicon.svg',
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '96x96',
+      url: '/favicon-96x96.png',
+    },
+    {
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      url: '/apple-touch-icon.png',
+    },
+  ],
+  authors: [
+    {
+      name: 'Fady Emad',
+      url: 'https://github.com/FadyEmad01',
+    },
+  ],
+  publisher: 'Fixel UI',
+  openGraph: {
+    title: 'Fixel UI',
+    description:
+      'Fully animated, open-source component distribution built with React, TypeScript, Tailwind CSS, Motion and Shadcn CLI. Browse a list of components you can install, modify, and use in your projects.',
+    url: 'http://localhost:3000/',
+    siteName: 'Fixel UI',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Fixel UI',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@Fixel_UI',
+    title: 'Fixel UI',
+    description:
+      'Fully animated, open-source component distribution built with React, TypeScript, Tailwind CSS, Motion and Shadcn CLI. Browse a list of components you can install, modify, and use in your projects.',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Fixel UI',
+      },
+    ],
+  },
+};
 
 export default function Layout({ children }: LayoutProps<'/'>) {
   return (
-    <html lang="en" className={inter.className} suppressHydrationWarning>
+    <html lang="en"
+      className={`${inter.className} ${syne.className} antialiased `}
+      suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
       </body>
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,9 +1,15 @@
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
+import { CodeBlock, CodeBlockProps, Pre } from './components/www/docs/codeblock';
 
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
     ...components,
+    pre: (props: CodeBlockProps) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
   };
 }
